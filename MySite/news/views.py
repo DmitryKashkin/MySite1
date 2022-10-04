74cb30a5f2928863b80ea80d11f3161b926b2ad9
DmitryKashkin <7842066@gmail.com>
2022-10-04 20:58:24 +0300

    v0.1

--- MySite/news/views.py
+++ MySite/news/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 def index(request):
-    # print(request.META)
-    return HttpResponse('<h1>Hello world!</h1')
-
-
-def test(request):
-    # print(request.META)
-    return HttpResponse('<h2>Test page</h2')
+    news = News.objects.order_by('-created_at')
+    context = {'news': news, 'title': 'News list'}
+    return render(request, 'news/index.html', context)
