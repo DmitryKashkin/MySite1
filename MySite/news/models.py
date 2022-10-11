bd4238703eb0e254ffdd7e7a05825ea5373041c3
DmitryKashkin <7842066@gmail.com>
2022-10-11 20:41:58 +0300

    36

--- MySite/news/models.py
+++ MySite/news/models.py
@@ -9,7 +9,10 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    category = models.ForeignKey(
+        'Category', on_delete=models.PROTECT, verbose_name='Категория'
+    )
+    views = models.IntegerField(default=0, verbose_name='Количество просмотров')
 
     def __str__(self):
         return self.title
