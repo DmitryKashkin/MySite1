be27a3f54fa8bb65d6bd77f650ba851e98656e43
DmitryKashkin <7842066@gmail.com>
2022-10-12 20:46:08 +0300

    v.41

--- MySite/news/admin.py
+++ MySite/news/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('category', 'is_published')
     list_filter = ('category', 'is_published')
+    fields = (
+        'title', 'category', 'content', 'photo', 'get_photo', 'is_published', 'views', 'created_at', 'updated_at')
+    readonly_fields = ('get_photo', 'views', 'created_at', 'updated_at')
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="150">')
+
+    get_photo.short_description = 'Фото'
 
 
 class CategoryAdmin(admin.ModelAdmin):
--- MySite/news/templatetags/news_tags.py
+++ MySite/news/templatetags/news_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from news.models import Category
 
@@ -11,6 +12,7 @@ def get_categories():
 
 
 @register.inclusion_tag('news/list_categories.html')
-def show_categories(arg1='Hello', arg2='world!'):
-    categories = Category.objects.all()
-    return {'categories': categories, 'arg1': arg1, 'arg2': arg2}
+def show_categories():
+    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
+    # categories = Category.objects.all()
+    return {'categories': categories,}
