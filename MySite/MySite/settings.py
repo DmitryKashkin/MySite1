0c959f3da067216d12f6999907bd481180caf424
DmitryKashkin <7842066@gmail.com>
2022-10-14 21:52:13 +0300

    v.45.5

--- MySite/MySite/settings.py
+++ MySite/MySite/settings.py
@@ -139,3 +139,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+LOGIN_REDIRECT_URL = 'home'
--- MySite/news/views.py
+++ MySite/news/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from .models import News, Category
 from .forms import NewsForm
 from .utils import MyMixin
@@ -70,7 +71,6 @@ class Regiser(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'news/register.html'
-
 
 # def index(request):
 #     news = News.objects.order_by('-created_at')
